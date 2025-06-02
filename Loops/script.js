d89917e0a6683eaf6eb4eae2ddaecd788075c1cf
vchhu <90860763+vchhu@users.noreply.github.com>
2025-06-01 23:45:03 -0400

    Create script.js
    Unfinished (WIP)

---
+++ Loops/script.js
@@ -0,0 +1,7 @@
+function submit() {
+  let randomizedNumber = 1;
+  let value = document.querySelector("input").value;
+  if (value < 1 || value > 100) {}
+  let resultDisplay = document.getElementById("result");
+  if (value == randomizedNumber) {}
+}
