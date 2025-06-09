fc7b5dbe6944031f083aa1752fa5ea1de127bc3c
vchhu <90860763+vchhu@users.noreply.github.com>
2025-06-08 20:34:03 -0400

    Update script.js

--- Arrays and Functions/script.js
+++ Arrays and Functions/script.js
@@ -1,1 +1,29 @@
-answers = ["Earth", "mars", "Jupiter", "Saturn"];
+const questions = [];
+const choices = ["Earth", "Mars", "Jupiter", "Saturn"];
+const answers = [];
+
+let currQNum = 0;
+let score = 0;
+
+function display() {
+  if (currQNum < questions.length) {
+    //Display question based off current num as index for qArray
+  }
+  else {
+    //Display score & restart button
+  }
+}
+
+function checkAnswer(btn) {
+  if (btn.value === answers[currQNum]) score++;
+  currQNum++;
+  display();
+}
+
+function restart() {
+  score = 0;
+  currQNum = 0;
+  display();
+}
+
+display();
