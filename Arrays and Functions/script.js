2afd9223ea2cada3be4831a230157fc7aa02b6e8
vchhu <90860763+vchhu@users.noreply.github.com>
2025-06-11 01:49:31 -0400

    Update script.js
    Should be finished - needs testing

--- Arrays and Functions/script.js
+++ Arrays and Functions/script.js
@@ -17,35 +17,48 @@ const answers = [
   "Da Vinci"
 ];
 
-const restartBtn =  document.createElement("button");
-restartBtn.innerHTML = "Play Again?";
-restartBtn.onclick = restart;
-const btnsParent = document.getElementById("choicesArea");
-
-let currQNum = 0;
+let currQ = 0;
 let score = 0;
 
 function display() {
-  let text = document.getElementById("questionArea");
-  if (currQNum < questions.length) {
+  var text = document.getElementById("questionArea");
+  var btnsParent = document.getElementById("choicesArea");
+  if (currQ < questions.length) {
     //Display question based off current num as index for qArray
+    text.innerHTML = questions[currQ];
+    for (let i = 0; i < 4; i++) {
+      let btn = document.getElementById('choice${i}');
+      btn.innerHTML = choices[currQ][i];
+      btn.value = choices[currQ][i];
+    }
   }
   else {
     //Display score & restart button
-    text.innerHTML = 'You scored ${score} out of ${currQNum}';
-    
+    text.innerHTML = 'You scored ${score} out of ${currQ}';
+    for (let i = 0; i < 4; i++) btnsParent.removeChild();
+    let restartBtn = document.createElement("button");
+    restartBtn.innerHTML = "Play Again?";
+    restartBtn.onclick = restart;
+    btnsParent.appendChild(restartBtn);
   }
 }
 
 function checkAnswer(btn) {
-  if (btn.value === answers[currQNum]) score++;
-  currQNum++;
+  if (btn.value === answers[currQ]) score++;
+  currQ++;
   display();
 }
 
 function restart() {
   score = 0;
-  currQNum = 0;
+  currQ = 0;
+  var btnsParent = document.getElementById("choicesArea");
+  btnsParent.removeChild();
+  for (let i = 0; i < 4; i++) {
+    let btn = document.createElement("button");
+    btn.id = 'choice${i}';
+    btnsParent.appendChild(btn);
+  }
   display();
 }
 
