d0c34c55b000d6df3d78b6084b1252492d11d58c
vchhu <90860763+vchhu@users.noreply.github.com>
2025-07-14 19:29:40 -0400

    fixed import

--- Intro to React/index.jsx
+++ Intro to React/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { ScoreKeeper } from "./ScoreKeeper.jsx";
+import { ScoreKeeper } from "./ScoreKeeper";
 
 const ScoreBoard = () => {
     return (
